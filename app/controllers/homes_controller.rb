a5afdca973c8cc12c0e73a252fdc938f38cea839
Kraziion2 <supersamiec2012@gmail.com>
2019-03-27 04:25:51 -0700

    fixed profile not being created

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,18 @@
 class HomesController < InheritedResources::Base
-
+	before_action :authenticate_user!
 	def index
 		@articles=Article.all
 	end	
+
+	def home
+		if user_signed_in?
+			if Profile.find_by(user_id:current_user.id)
+			else
+			redirect_to new_profile_path
+			end	
+		end
+
+  	end
   private
 
     def home_params
--- config/routes.rb
+++ config/routes.rb
@@ -21,10 +21,10 @@ Rails.application.routes.draw do
   resources :profiles
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root 'homes#homepage'
+  root 'homes#home'
   get 'profiles/new'
   get 'homes/index'
-  get 'homes/homepage' 
+  get 'homes/home' 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
